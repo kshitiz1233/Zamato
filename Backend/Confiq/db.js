ad12a1c57e2d471a8528d3b6f26c618624595bd2
kshitiz1233 <awasthi.kshitiz28@gmail.com>
2026-07-12 17:11:51 +0530

    Use environment variables for database configuration

--- Backend/Confiq/db.js
+++ Backend/Confiq/db.js
@@ -1,11 +1,17 @@
-const Sequelize = require('sequelize');
+require("dotenv").config();
 
-const db = new Sequelize('zamato', 'root', 'Kshitiz@28', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false,
-});
+const Sequelize = require("sequelize");
 
- 
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = db;
